1dc9ad28a01d4496bf261276a154cafb1cc17874
Thayna <thayna.caixeta@gmail.com>
2020-06-10 10:08:51 -0300

    Add an index page
    Also add an slider to improve the ux of the index page

--- Typer/servidor/config/express.js
+++ Typer/servidor/config/express.js
@@ -6,17 +6,17 @@ var app = express();
 app.use(express.static('./../public'));
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// app.use(function(req, res, next) {
-// 	res.header("Access-Control-Allow-Origin", "*");
-// 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-// 	next();
-// });
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
-consign({cwd: 'app'})
-	.include('api')
-	.then('routes')
-	.into(app);
+consign({ cwd: 'app' })
+    .include('api')
+    .then('routes')
+    .into(app);
 
 module.exports = app;
